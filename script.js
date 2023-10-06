b7b9e3c6f97e631373aa1674e3e1d962a276ce54
szt2palpat <144673180+szt2palpat@users.noreply.github.com>
2023-10-06 08:39:54 +0200

    dun not working

--- script.js
+++ script.js
@@ -1,53 +1,5 @@
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-const stylesheet = document.getElementById("stylesheet");
-const darkStylesheet = document.getElementById("dark-stylesheet");
-
-const isDarkMode = localStorage.getItem("darkMode") === "enabled";
-
-
-if (isDarkMode) {
-    enableDarkMode();
+let theme=true;
+let t=document.body;
+if(theme){
+    
 }
-
-
-function enableDarkMode() {
-    document.body.classList.add("dark-mode");
-    stylesheet.disabled = true;
-    darkStylesheet.disabled = false;
-    localStorage.setItem("darkMode", "enabled");
-    darkModeToggle.checked = true;
-}
-
-
-function disableDarkMode() {
-    document.body.classList.remove("dark-mode");
-    stylesheet.disabled = false;
-    darkStylesheet.disabled = true;
-    localStorage.setItem("darkMode", "disabled");
-    darkModeToggle.checked = false;
-}
-
-
-darkModeToggle.addEventListener("change", () => {
-    if (darkModeToggle.checked) {
-        enableDarkMode();
-    } else {
-        disableDarkMode();
-    }
-});
-
-
-function setSiteColors() {
-    if (document.body.classList.contains("dark-mode")) {
-        document.body.style.setProperty("--background-color", "#333"); 
-        document.body.style.setProperty("--text-color", "#fff"); 
-      
-    } else {
-        document.body.style.setProperty("--background-color", "#f4f4f4"); 
-        document.body.style.setProperty("--text-color", "#000"); 
-        
-    }
-}
-
-
-setSiteColors();
